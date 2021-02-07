b0462b170b0418e48314e167e782559e174be413
Sai Kiran Voleti <78673507+KiranSai-Developer@users.noreply.github.com>
2021-02-07 15:33:34 +0530

    DSA Day 1 Assignment 1

---
+++ DSA Day 1 Assignment.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#sum of two numbers
+a=5
+b=6
+c=a+b
+print(c)
+
+
+# In[2]:
+
+
+#subtraction of two numbers
+a=85
+b=65
+c=a-b
+print(c)
+
+
+# In[3]:
+
+
+#multiplication of two numbers
+a=8
+b=9
+c=a*b
+print(c)
+
+
+# In[4]:
+
+
+#division of two numbers
+a=60
+b=30
+c=a/b
+print(c)
+
+
+# In[5]:
+
+
+x =5
+y = 10
+z=x^y 
+print(z)
+
+
+# In[ ]:
+
+
+
+
